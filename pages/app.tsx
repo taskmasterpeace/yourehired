d9f54f72d3931a0db446947483489367c64f6a87
Your Name <you@example.com>
2025-03-16 15:21:27 -0400

    refactor: Remove redundant app and index pages

--- pages/app.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic';
-
-// Import the main application component with SSR disabled
-const CAPTAINGui = dynamic(() => import('@/captain_gui'), { ssr: false });
-
-export default function AppPage() {
-  return <CAPTAINGui />;
-}
